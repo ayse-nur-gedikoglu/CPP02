7645ea3b537712c2d222e5a3e00f271a48013341
ayse-nur-gedikoglu <gedikoglu_27@icloud.com>
2026-02-25 16:50:04 +0300

    ex00 yapısı olusturuldu

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # CPP02
+
+Ad-hoc polymorphism, operator overloading and the Orthodox Canonical class form
+
+ex00: Orthodox Canonical Class Form Nedir?
+
+Doğru ve güvenli nesne kopyalama ve imha sağlayan üç özel üye işlevinden oluşan standart settir:
+
+- Copy constructor
+MyClass(const MyClass& other);
+
+
+- Copy assignment operator
+MyClass& operator=(const MyClass& other);
+
+- Destructor
+~MyClass();
+
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -3,7 +3,17 @@
 
 class Fixed
 {
-
+    private:
+        int fixed_point;
+        static const int fractional_bits = 8;
+    public:
+        Fixed(); //default constructor
+        ~Fixed(); //destructor
+        Fixed(const Fixed &other); //copy constructor
+        Fixed &operator=(const Fixed &other); //copy assignment operator
+        
+        int getRawBits(void) const;
+        void setRawBits(int const raw);
 };
 
 #endif
